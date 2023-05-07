bab7ffcc71b4ac86369a450d929c4b34276b10f7
Stella <live.hylia@outlook.com>
2023-05-06 22:55:56 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# File Organizer
+
+This Python script organizes files in a directory based on their file type.
+
+## Prerequisites:
+- Python 3.10.11 or higher
+
+## Installation:
+1. Clone this repository to your local machine using the following command:
+    ```bash
+    git clone https://github.com/0xhylia/file-organizer.git
+    ```
+2. Navigate to the cloned repository directory.
+
+## Usage:
+1. Open a terminal window.
+2. Navigate to the cloned repository directory.
+3. Run the script using the following command: `python file_organizer.py`
+4. Enter the path to the folder you want to organize.
+5. If the path is valid, the script will ask for your confirmation to proceed with the file organization.
+6. Enter "y" to proceed with the file organization.
+7. The script will organize the files in the directory based on their file type and create folders for each file type.
+8. Once the files are organized, the script will output "Done!".
+
+## File Types
+The script organizes files into the following categories based on their file type:
+
+- Images
+- Videos
+- Audio
+- Documents
+- Archives
+- Disc
+- Database
+- Executables
+- Fonts
+- Presentations
+- Programming
+- Spreadsheets
+- System
+- Web
+- Disk Image
+- Shortcut
+
+## Contributing
+If you find any issues with the script or want to add new features, feel free to open an issue or submit a pull request.
---
+++ file_organizer.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import time
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+types = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".svg", "webp"],
+    "Videos": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
+    "Audio": [".mp3", ".wav", ".ogg", ".m4a", ".m4b", ".m4p", ".m4r", ".aac", ".aiff", ".wma", ".flac", ".alac"],
+    "Documents": [".doc", ".docx", ".pdf", ".odt", ".xls", ".xlsx", ".ods", ".ppt", ".pptx", ".txt", ".rtf", ".tex"],
+    "Archives": [".zip", ".7z", ".rar", ".tar", ".gz", ".pkg", ".deb"],
+    "Disc": [".iso", ".img", ".dmg", ".toast", ".vcd"],
+    "Database": [".sql", ".csv", ".dat", ".db", ".dbf", ".log"],
+    "Executables": [".exe", ".apk", ".bat", ".cgi", ".pl", ".com", ".gadget", ".jar", ".msi", ".wsf"],
+    "Fonts": [".ttf", ".otf", ".fon", ".fnt"],
+    "Presentations": [".key", ".odp", ".pps", ".pptx"],
+    "Programming": [".py", ".c", ".class", ".cpp", ".cs", ".h", ".java", ".sh", ".swift", ".vb", ".js", ".php", ".html", ".css", ".scss", ".less", ".json", ".go", ".pl", ".lua", ".rb", ".ts", ".as", ".vb", ".xhtml", ".jsp", ".jspx", ".wss", ".do", ".action", ".pl", ".cgi", ".php4", ".php3", ".phtml", ".py", ".pyc", ".pyo", ".rhtml", ".shtml", ".xml", ".rss", ".svg"],
+    "Spreadsheets": [".ods", ".xls", ".xlsm", ".xlsx"],
+    "System": [".bak", ".cab", ".cfg", ".cpl", ".cur", ".dll", ".dmp", ".drv", ".icns", ".ico", ".ini", ".lnk", ".msi", ".sys", ".tmp", ".pem"],
+    "Web": [".asp", ".aspx", ".cer", ".cfm", ".csr", ".css", ".dcr", ".htm", ".html", ".js", ".jsp", ".php", ".rss", ".xhtml", ".cgi", ".pl", ".py", ".xhtml", ".rss", ".xml", ".js", ".jsp", ".jspx", ".wss", ".do", ".action", ".pl", ".cgi", ".php4", ".php3", ".phtml", ".py", ".pyc", ".pyo", ".rhtml", ".shtml", ".xml", ".rss", ".svg"],
+    "Disk Image": [".bin", ".dmg", ".iso", ".toast", ".vcd"],
+    "Shortcut": [".lnk", ".url"],
+    "Torrent": [".torrent"],
+    "Settings": [".cfg", ".ini", ".prf", ".env", ".properties", ".settings", ".theme", ".plist", ".reg", ".sif"],
+    "Web Browser": [".bookmark", ".crdownload"],
+    "Unity": [".unitypackage", ".vrm"],
+}
+
+
+clear()
+print("Enter the path to the folder you want to organize: ")
+path = input()
+
+
+if not os.path.exists(path):
+    print("The path does not exist")
+    sys.exit()
+
+files = os.listdir(path)
+
+print("Are you sure you want to organize the files in this folder? (y/n)")
+choice = input()
+
+if choice.lower() == "y":
+    print("Organizing files...")
+    intial_file_count = len(files)
+    files_organized = 0
+
+    for file in files:
+        if file != os.path.basename(__file__):
+            for type in types:
+                if file.endswith(tuple(types[type])):
+                    if not os.path.exists(path + "/" + type):
+                        os.mkdir(path + "/" + type)
+                    os.rename(path + "/" + file, path + "/" + type + "/" + file)
+                    files_organized += 1
+                    print(str(files_organized) + " / " + str(intial_file_count))
+                    time.sleep(1)
+                    break
+
+    print("Done!")
+
+elif choice.lower() == "n":
+    print("Exiting...")
+    sys.exit()
+
+else:
+    print("Invalid input please try again [y/n]")
+    sys.exit()
