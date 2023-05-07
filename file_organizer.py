a358a1a8424f9648a1d306383d88e9d756bfd939
kiyolol <predevxyzlol@gmail.com>
2023-05-07 18:34:18 -0400

    This should work...

--- file_organizer.py
+++ file_organizer.py
@@ -2,8 +2,55 @@ import os
 import sys
 import time
 import requests
+import datetime
 
 version = "1.0.2"
+args = sys.argv[1:]
+
+def getActions():
+        print("Choose a action:")
+
+        actions = []
+        for folder in os.listdir("logs"):
+            for file in os.listdir("logs/" + folder):
+                if file == "actions.txt":
+                    actions.append(datetime.datetime.fromtimestamp(int(folder)).strftime("%d/%m/%Y %H:%M:%S"))
+
+        for i in range(len(actions)):
+            print(str(i + 1) + ". " + actions[i])
+
+        action = input("Action: ")
+
+        if action.isdigit():
+            action = int(action)
+            for folder in os.listdir("logs"):
+                for file in os.listdir("logs/" + folder):
+                    if file == "actions.txt":
+                        if datetime.datetime.fromtimestamp(int(folder)).strftime("%d/%m/%Y %H:%M:%S") == actions[action - 1]:
+                            act = open("logs/" + folder + "/actions.txt", "r")
+                            print(act.read())
+                            act.close()
+                            sys.exit()
+
+        
+
+
+def createactions(actions):
+    timestamp = str(int(time.time()))
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    if not os.path.exists("logs/" + timestamp):
+        os.mkdir("logs/" + timestamp)
+    
+    act = open("logs/" + timestamp + "/actions.txt", "w+")
+
+    for action in actions:
+        act.write("[ " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " ] " + action + "\n")
+
+    act.close()
+        
+
+
 
 def check_for_updates():
     response = requests.get("https://api.github.com/repos/0xhylia/file-organizer/releases/latest")
@@ -22,7 +69,7 @@ def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 
-args = sys.argv[1:]
+
 
 
 types = {
@@ -53,6 +100,10 @@ if len(args) > 0 and args[0] == "--list":
         print(type + ": " + str(types[type]))
     sys.exit()
 
+
+if len(args) > 0 and args[0] == "--actions":
+    getActions()
+
 def get_files(path):
     files = []
     for file in os.listdir(path):
@@ -64,52 +115,62 @@ def get_files(path):
                 
     return files
 
-if check_for_updates() != True:
-    print(check_for_updates())
-    sys.exit()
-clear()
-print("Welcome to File Organizer!")
-print("Version: " + version)
-print("Author: 0xhylia")
-print("Github: https://github.com/0xhylia/file-organizer")
-print("Enter the path to the folder you want to organize: ")
-path = input()
+
+things_did = []
+
+def main():
+
+    if check_for_updates() != True:
+        print(check_for_updates())
+        sys.exit()
+    clear()
+    print("Welcome to File Organizer!")
+    print("Version: " + version)
+    print("Author: 0xhylia")
+    print("Github: https://github.com/0xhylia/file-organizer")
+    print("Enter the path to the folder you want to organize: ")
+    path = input()
 
 
-if not os.path.exists(path):
-    print("The path does not exist")
-    sys.exit()
+    if not os.path.exists(path):
+        print("The path does not exist")
+        sys.exit()
 
-files = get_files(path)
+    files = get_files(path)
 
 
 
-print("Are you sure you want to organize the files in this folder? (y/n)")
-choice = input()
+    print("Are you sure you want to organize the files in this folder? (y/n)")
+    choice = input()
 
-if choice.lower() == "y":
-    print("Organizing files...")
-    intial_file_count = len(files)
-    files_organized = 0
+    if choice.lower() == "y":
+        print("Organizing files...")
+        intial_file_count = len(files)
+        files_organized = 0
 
-    for file in files:
-        if file != os.path.basename(__file__):
-            for type in types:
-                if file.endswith(tuple(types[type])):
-                    if not os.path.exists(path + "/" + type):
-                        os.mkdir(path + "/" + type)
-                    os.rename(path + "/" + file, path + "/" + type + "/" + file)
-                    files_organized += 1
-                    print(str(files_organized) + " / " + str(intial_file_count))
-                    time.sleep(1)
-                    break
+        for file in files:
+            if file != os.path.basename(__file__):
+                for type in types:
+                    if file.endswith(tuple(types[type])):
+                        if not os.path.exists(path + "/" + type):
+                            os.mkdir(path + "/" + type)
+                        os.rename(path + "/" + file, path + "/" + type + "/" + file)
+                        files_organized += 1
+                        print(str(files_organized) + " / " + str(intial_file_count))
+                        time.sleep(1)
+                        things_did.append("Moved " + file + " to " + type + " folder")
+                        break
 
-    print("Done!")
+        print("Done!")
+        createactions(things_did)
 
-elif choice.lower() == "n":
-    print("Exiting...")
-    sys.exit()
+    elif choice.lower() == "n":
+        print("Exiting...")
+        sys.exit()
 
-else:
-    print("Invalid input please try again [y/n]")
-    sys.exit()
+    else:
+        main()
+
+
+if __name__ == "__main__":
+    main()
